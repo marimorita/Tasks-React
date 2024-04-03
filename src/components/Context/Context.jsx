bbf883c5b7ec339d333423ff8297838ab9600d8f
unknown <SenaTarde@ARMAPRRQDFSD203.FormacionSena.RED>
2024-04-03 17:33:17 -0500

    solution bug

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -2,7 +2,7 @@ import {createContext, useState} from "react" //Aca se importa el useState y el 
 export const tasksContext = createContext() //Aca se crea el contexto
 
 //Esta función es para proveer las tareas 
-export const tasksProvider  = ({children}) =>{ //El children va a utilizar los valores de provider
+export const TasksProvider  = ({children}) =>{ //El children va a utilizar los valores de provider
     const [tasks, setTasks] = useState ([]); //Esto son los estados de la función 
     return(
         <tasksContext.Provider value = {{tasks, setTasks}}>{children}
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { tasksProvider } from './components/Context/Context.jsx'
+import { TasksProvider } from './components/Context/Context.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<tasksProvider><App /></tasksProvider>) //Este proveedor se pone aca, para que sea global en mi proyecto
+ReactDOM.createRoot(document.getElementById('root')).render(<TasksProvider> <App /> </TasksProvider>) //Este proveedor se pone aca, para que sea global en mi proyecto
